10cf415b59ae205b3d2cb614e47d0fef70441eaa
Razvan Gavril <razvan.gavril@gmail.com>
2020-02-26 16:45:15 +0000

    Fixy fixy

--- MIoTAirPurifier.js
+++ MIoTAirPurifier.js
@@ -14,7 +14,7 @@ class MIoTAirPurifier extends MIoTDevice {
             'filter_level' : [ 4,  3], // Filter Live Level : 0-100(Percentage)
             'lock'         : [ 7,  1], // Physical Control Locked : bool
             'speed_write'  : [10,  8], // motor1-speed : 0-3000 : 0-3000 1
-            'speed_read'   : [10,  9]  // motor1-speed : 0-3000 : 0-3000 1
+            'speed_read'   : [10,  9], // motor1-speed : 0-3000 : 0-3000 1
             'aqi_heartbeat': [13,  9]  // aqi-updata-heartbeat: 0 - 65534
         }
 
